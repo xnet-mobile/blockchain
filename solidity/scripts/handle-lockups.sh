fcad0e587ed24da9ce17c41239b6b38191c57000
Richard DeVaul <richard.devaul@gmail.com>
2023-01-31 14:11:01 -0800

    minor bugfixes, principally in the handling of amount=0 in slash(addres token, uint256 amount) in XNETLockup2.sol

--- solidity/scripts/handle-lockups.sh
+++ solidity/scripts/handle-lockups.sh
@@ -2,26 +2,30 @@
 echo "**********************************************************"
 echo "==> processing new XNET lockups on network $NETWORK"
 echo "**********************************************************"
-if [ "${NETWORK}" = "" ]; then
+if [ "${NETWORK}" == "" ]; then
     echo "unset NETWORK -- exiting"
     exit 1
 fi
 echo "deploying new vesting wallet contracts, if any"
-npx hardhat run --network $NETWORK scripts/deploy-XNETLockup2.js
 
-echo "pausing"
-sleep 10
+if [ ${1} == "deploy" ]; then
+    echo "deploying contracts"
+    npx hardhat run --network $NETWORK scripts/deploy-XNETlockup.js
+    echo "pausing"
+    sleep 10
+fi
 
 echo "verifying new vesting wallet contracts, if any"
 while read -r line; do
     echo $line;
     ADDR=`echo $line | awk '{print $1}'`
-    BEN=`echo $line | awk '{print $2}'`
-    START=`echo $line | awk '{print $3}'`
-    DUR=`echo $line | awk '{print $4}'`
-    npx hardhat verify --contract contracts/XNETLockup2.sol:XNETLockup2 --network ${NETWORK} $ADDR $BEN $START $DUR
+    AGENT=`echo $line | awk '{print $2}'`
+    BEN=`echo $line | awk '{print $3}'`
+    START=`echo $line | awk '{print $4}'`
+    DUR=`echo $line | awk '{print $5}'`
+    npx hardhat verify --contract contracts/XNETlockup.sol:XNETLockup --network ${NETWORK} $ADDR $BEN $AGENT $START $DUR
     echo "pausing"
     sleep 2
-done < new-deploys.${NETWORK}.json
+done < lockup2-new-deploys.${NETWORK}.txt
 
 echo "done"
