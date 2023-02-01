b75286068ab0151678050ea757788844de1ade4c
Sint Connexa <connexa@xnet.company>
2023-02-01 11:21:28 -0800

    minor edits and additional documentation

--- solidity/hardhat.config.js
+++ solidity/hardhat.config.js
@@ -6,11 +6,23 @@ require('@nomiclabs/hardhat-ethers');
 require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
-// stick secrets in environment variables
-// richard@xnet.company Alchemy account
+// We are using Alchemy for deployment to non-localhost test and
+// production networks.  To do this, you will need to create an
+// Alchemy account (go to https://www.alchemy.com/) and get your API
+// keys.
+
+// Also, to do a deployment to a non-localhost network, you will need
+// to know the private key of the account you are deploying from --
+// for example, you can get your private key from your MetaMask wallet.
+
+// Export the plaintext of your private key and get your Alchemy API
+// keys, then edit the .env file and make the appropriate
+// subsitutions.
+
+// DO NOT CHECK YOUR ACTUAL CREDENTIALS INTO THE GIT REPOSITORY
 
 const { PRIVATE_KEY, POLYGON_API_URL, MUMBAI_API_URL, GOERLI_API_URL,
-	ETHER_GOERLI_API, ETHER_POLYGON_API} = process.env;
+	ETHER_GOERLI_API, ETHER_POLYGON_API, ETHER_MUMBAI_API} = process.env;
 
 var network = process.env.NETWORK;
 if (network == undefined) {
@@ -23,6 +35,9 @@ var etherKey = null;
 switch (network) {
 case 'goerli':
     etherKey = `${ETHER_GOERLI_API}`;
+    break
+case 'mumbai':
+    etherKey = `${ETHER_MUMBAI_API}`;
     break
 case 'polygon':
 default:
